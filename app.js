8ce2a527091de502b245cec0af8272928d6a62f5
Dieumerci Muhindo <dieumerci.md09@gmail.com>
2022-07-15 17:12:41 +0200

    feat: add a helper folder

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-
 const express = require('express')
+const {success} = require('./helper')
 let listPockemon = require('./listPockemon')
 
 const app = express()
@@ -8,16 +8,20 @@ const port = 5000
 app.get('/',(req,res) => {
     res.send(`<p> je sui là </p>`)
 })
-// app.get('/api/pokemon/:id' , (req,res) => {
-//     const recupererid = parseInt(req.params.id)
-//     const pokemon = list_pockemon.find( (pokemon) => list_pockemon.id === recupererid)
-//     res.send(`<h1> Voici ${list_pockemon.nom} ton Pokemon</h1>`)
-// } )
+app.get('/api/pokemon/:id' , (req,res) => {
+    const id = parseInt(req.params.id)
+    res.send(`<h1> Voici ${listPockemon[id]} ton Pokemon</h1>`)
+} )
 
-// app.get('/api/pokemon', (req,res) =>{
+app.get('/api/pokemon', (req,res) =>{
 
-//     const numbPockemon = listPockemon.length
-//     res.send(`nous avons ${numbPockemon} Pockemons pour l'instant`)
+    const numbPockemon = listPockemon.length
+    res.send(`nous avons ${numbPockemon} Pockemons pour l'instant`)
 
+})
+// app.get('/api/pockemon/:id' , (req , res) =>{
+//     const id = parseInt(req.params.id)
+//     //const message  = `Voici la list de tous les pockemons`
+//     res.json(listPockemon[id])
 // })
 app.listen(port, ()=> console.log( `Notre app est lancée sur : http://localhost:${port}`))
